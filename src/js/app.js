82318b4247a66c6e21e1d6660c14a2a3bf377941
saket pareek <saket.wdev@gmail.com>
2018-12-14 10:08:37 -0800

    add js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 
+let ViewModel = function() {
+    this.toggleSidebar = function() {
+      $('body').toggleClass('sidebar--hidden');
+      $('.home__content').toggleClass('home__content--shift');
+      $('.home__content__foreground').toggleClass('home__content__foreground--dark');
+    }
+  }
+  
+  ko.applyBindings(new ViewModel());
